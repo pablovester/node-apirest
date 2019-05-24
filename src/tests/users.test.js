03f4a6fab6d86ebfd363298e03645aea45a14dfd
Pablo Gerez V <pablo.gerez@globant.com>
2019-05-24 15:29:24 -0300

    GET USERS test suite

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -4,24 +4,47 @@ const models = require('../api-server/db/models')
 
 describe('Listing all users', () => {
 
-    it('GET all users', async (done) => {
-        
+    let mockRequest = {
+        body: {}
+    };
+
+    let mockResponse = {
+        status: jest.fn(),
+        send: jest.fn(),
+        json: jest.fn()
+    };
+
+    it('GET all users successfully', async (done) => {
         models.User.findAll = await jest.fn().mockReturnValue(['1', '2', '3']);
-        let mockResponse = {
-            status: jest.fn(),
-            send: jest.fn(),
-            json: jest.fn()
-        };
+
         mockResponse.status.mockReturnValue(mockResponse);
         mockResponse.json.mockReturnValue(mockResponse);
-
-        let mockRequest = {
-            body: {}
-        };
 
         const users = await usersController.getUsers(mockRequest, mockResponse);
         expect(mockResponse.status).toHaveBeenCalledWith(200);
         done();
     });
-})
+
+    it('GET all users with failure', async (done) => {
+        models.User.findAll = await jest.fn().mockImplementation(() => { throw new Error('FAILED') });
+        
+        mockResponse.status.mockReturnValue(mockResponse);
+        mockResponse.json.mockReturnValue(mockResponse);
+
+        const users = await usersController.getUsers(mockRequest, mockResponse);
+        expect(mockResponse.status).toHaveBeenCalledWith(500);
+        done();
+    });
+
+    it('GET all users and users list is empty', async (done) => {
+        models.User.findAll = await jest.fn().mockReturnValue(null);
+
+        mockResponse.status.mockReturnValue(mockResponse);
+        mockResponse.json.mockReturnValue(mockResponse);
+
+        const users = await usersController.getUsers(mockRequest, mockResponse);
+        expect(mockResponse.status).toHaveBeenCalledWith(204);
+        done();
+    })
+});
 
